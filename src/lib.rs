83bfd4bba0fe3d791a6637699b9e736959876dcc
SaiYS <yutapypy0930@gmail.com>
2022-03-29 07:59:00 +0900

    add prelude

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,19 @@
-pub use graph::list_graph::{DirectedListGraph, UndirectedListGraph};
-pub use itertools_sail::ItertoolsSail as _;
-pub use modint::{ModInt1000000007, ModInt998244353};
-pub use prime::{
-    miller_rabin::MillerRabin, sieve_of_atkin::SieveOfAtkin,
-    sieve_of_eratosthenes::SieveOfEratosthenes, trial_division::TrialDivision,
-};
-pub use union_find::UnionFind;
-pub use vis::{vis, visualize::Visualize, Yn};
+pub mod prelude {
+    pub use fenwick_tree::FenwickTree;
+    pub use graph::list_graph::{DirectedListGraph, UndirectedListGraph};
+    pub use itertools_sail::ItertoolsSail as _;
+    pub use modint::{ModInt1000000007, ModInt998244353};
+    pub use prime::{miller_rabin::MillerRabin as _, sieve_of_atkin::SieveOfAtkin};
+    pub use segment_tree::{monoid::Monoid, SegmentTree};
+    pub use union_find::UnionFind;
+    pub use vis::{vis, visualize::Visualize, Yn};
+}
+
+pub use fenwick_tree::*;
+pub use graph::*;
+pub use itertools_sail::*;
+pub use modint::*;
+pub use prime::*;
+pub use segment_tree::*;
+pub use union_find::*;
+pub use vis::*;
