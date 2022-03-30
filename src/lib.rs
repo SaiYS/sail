843ba716421d7c5887215b1b28478644eab30881
SaiYS <yutapypy0930@gmail.com>
2022-03-31 07:57:33 +0900

    fix export prime

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ pub mod prelude {
     pub use graph::list_graph::{DirectedListGraph, UndirectedListGraph};
     pub use itertools_sail::ItertoolsSail as _;
     pub use modint::{ModInt1000000007, ModInt998244353};
-    pub use prime::{miller_rabin::MillerRabin as _, sieve_of_atkin::SieveOfAtkin};
+    pub use prime::{miller_rabin::MillerRabin as _, sieve::atkin::SieveOfAtkin};
     pub use segment_tree::{monoid::Monoid, SegmentTree};
     pub use union_find::UnionFind;
     pub use vis::{vis, visualize::Visualize, Yn};
