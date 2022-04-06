f0525c56c7028dbb56e6b9081abc3427835ab8cc
SaiYS <yutapypy0930@gmail.com>
2022-04-06 14:54:58 +0900

    ignore tests

--- tests/playground.rs
+++
@@ -1,9 +0,0 @@
-#[test]
-fn feature() {
-    let mut st = segment_tree::SegmentTree::<algebraic_structures::monoid::MSum<usize>>::new(10);
-    for i in 0..10 {
-        st.update(i, i);
-    }
-
-    st.dbg_tree()
-}
