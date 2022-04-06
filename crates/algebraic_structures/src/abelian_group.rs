aa1a5f0bf910b41018cf2c4f84e2451e803cbb86
SaiYS <yutapypy0930@gmail.com>
2022-04-06 15:37:40 +0900

    fix ableian_group

--- crates/algebraic_structures/src/abelian_group.rs
+++ crates/algebraic_structures/src/abelian_group.rs
@@ -1,6 +1,15 @@
+use std::ops::{Add, Mul, Neg};
+
+use num_traits::{Inv, One, Zero};
+
 use super::group::{GProduct, GSum, Group};
 
 pub trait AbelianGroup: Group {}
 
+impl<T: Clone + Zero + Add<Output = T> + Neg<Output = T>> AbelianGroup for GSum<T> {}
+
 pub type ASum<T> = GSum<T>;
+
+impl<T: Clone + One + Mul<Output = T> + Inv<Output = T>> AbelianGroup for GProduct<T> {}
+
 pub type AProduct<T> = GProduct<T>;
