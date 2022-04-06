95bfaefd80bc2a4185637404a55ceb0a47c71bb3
SaiYS <yutapypy0930@gmail.com>
2022-04-06 15:47:22 +0900

    modify fenwick_tree

--- crates/algebraic_structures/src/abelian_group.rs
+++ crates/algebraic_structures/src/abelian_group.rs
@@ -1,15 +1,13 @@
-use std::ops::{Add, Mul, Neg};
-
-use num_traits::{Inv, One, Zero};
-
 use super::group::{GProduct, GSum, Group};
+use num_traits::{Inv, One, Zero};
+use std::ops::{Add, Mul, Neg};
 
 pub trait AbelianGroup: Group {}
 
-impl<T: Clone + Zero + Add<Output = T> + Neg<Output = T>> AbelianGroup for ASum<T> {}
+impl<T: Clone + Zero + Add<Output = T> + Neg<Output = T>> AbelianGroup for GSum<T> {}
 
 pub type ASum<T> = GSum<T>;
 
-impl<T: Clone + One + Mul<Output = T> + Inv<Output = T>> AbelianGroup for AProduct<T> {}
+impl<T: Clone + One + Mul<Output = T> + Inv<Output = T>> AbelianGroup for GProduct<T> {}
 
 pub type AProduct<T> = GProduct<T>;
