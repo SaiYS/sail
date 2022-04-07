b48923248a7d53584bc0a426d6793d7d0837f876
SaiYS <yutapypy0930@gmail.com>
2022-04-08 02:21:58 +0900

    fix sample_gen

--- crates/sample_generater/src/lib.rs
+++ crates/sample_generater/src/lib.rs
@@ -1,62 +1,145 @@
-#![allow(unused_mut, unused_variables)]
-
 #[macro_export]
 macro_rules! gen {
-    () => {};
+    (@rng [$rng:expr]) => {};
 
-    (@rng [$rng:expr] @rest $(,)?) => {};
-
-    (@rng [$rng:expr] @rest, $var:ident = $lower:expr => $upper:expr) => {
-        let $var = rand::Rng::gen_range(&mut $rng, $lower, $upper);
+    // excluded array
+    (@rng [$rng:expr] $name:tt = [$lower:tt..$upper:tt; $rep:tt]) => {
+        let $name = (0..$rep).map(|_| rand::Rng::gen_range($rng, $lower, $upper)).collect::<Vec<_>>();
     };
-
-    (@rng [$rng:expr] @rest, $var:ident = [$lower:expr => $upper:expr; $rep:expr]) => {
-        let $var = (0..$rep).map(|_| rand::Rng::gen_range(&mut $rng, $lower, $upper)).collect::<Vec<_>>();
-    };
-
-    (@rng [$rng:expr] @rest, $var:ident = $val:expr) => {
-        let $var = $val;
-    };
-
-    (@rng [$rng:expr] @rest, $var:ident = $lower:tt..$upper:tt $($rest:tt)*) => {
-        let $var = rand::Rng::gen_range(&mut $rng, $lower, $upper);
-        $crate::gen! {
+    (@rng [$rng:expr] $name:tt = [$lower:tt..$upper:tt; $rep:tt], $($rest:tt)*) => {
+        let $name = (0..$rep).map(|_| rand::Rng::gen_range($rng, $lower, $upper)).collect::<Vec<_>>();
+        gen! {
             @rng [$rng]
-            @rest $($rest)*
+            $($rest)*
+        }
+    };
+    (@rng [$rng:expr] mut $name:tt = [$lower:tt..$upper:tt; $rep:tt]) => {
+        let mut $name = (0..$rep).map(|_| rand::Rng::gen_range($rng, $lower, $upper)).collect::<Vec<_>>();
+    };
+    (@rng [$rng:expr] mut $name:tt = [$lower:tt..$upper:tt; $rep:tt], $($rest:tt)*) => {
+        let mut $name = (0..$rep).map(|_| rand::Rng::gen_range($rng, $lower, $upper)).collect::<Vec<_>>();
+        gen! {
+            @rng [$rng]
+            $($rest)*
         }
     };
 
-    (@rng [$rng:expr] @rest, $var:ident = [$lower:tt..$upper:tt; $rep:tt], $($rest:tt)*) => {
-        let $var = (0..$rep).map(|_| rand::Rng::gen_range(&mut $rng, $lower, $upper)).collect::<Vec<_>>();
-        $crate::gen! {
+    // included array
+    (@rng [$rng:expr] $name:tt = [$lower:tt..=$upper:tt; $rep:tt]) => {
+        let $name = (0..=$rep).map(|_| rand::Rng::gen_range($rng, $lower, $upper + 1)).collect::<Vec<_>>();
+    };
+    (@rng [$rng:expr] $name:tt = [$lower:tt..=$upper:tt; $rep:tt], $($rest:tt)*) => {
+        let $name = (0..$rep).map(|_| rand::Rng::gen_range($rng, $lower, $upper + 1)).collect::<Vec<_>>();
+        gen! {
             @rng [$rng]
-            @rest $($rest)*
+            $($rest)*
+        }
+    };
+    (@rng [$rng:expr] mut $name:tt = [$lower:tt..=$upper:tt; $rep:tt]) => {
+        let mut $name = (0..$rep).map(|_| rand::Rng::gen_range($rng, $lower, $upper + 1)).collect::<Vec<_>>();
+    };
+    (@rng [$rng:expr] mut $name:tt = [$lower:tt..=$upper:tt; $rep:tt], $($rest:tt)*) => {
+        let mut $name = (0..$rep).map(|_| rand::Rng::gen_range($rng, $lower, $upper + 1)).collect::<Vec<_>>();
+        gen! {
+            @rng [$rng]
+            $($rest)*
         }
     };
 
-    (@rng [$rng:expr] @rest, $var:ident = $val:expr, $($rest:tt)*) => {
-        let $var = $val;
-        $crate::gen! {
+    // permutation
+    (@rng [$rng:expr] $name:tt = [$lower:tt -> $upper:tt; $rep:tt]) => {
+        let $name = rand::seq::IteratorRandom::choose_multiple($lower..$upper + 1, $rng, $rep);
+    };
+    (@rng [$rng:expr] $name:tt = [$lower:tt -> $upper:tt; $rep:tt], $($rest:tt)*) => {
+        let $name = rand::seq::IteratorRandom::choose_multiple($lower..$upper + 1, $rng, $rep);
+        gen! {
             @rng [$rng]
-            @rest $($rest)*
+            $($rest)*
+        }
+    };
+    (@rng [$rng:expr] mut $name:tt = [$lower:tt -> $upper:tt; $rep:tt]) => {
+        let mut $name = rand::seq::IteratorRandom::choose_multiple($lower..$upper + 1, $rng, $rep);
+    };
+    (@rng [$rng:expr] mut $name:tt = [$lower:tt -> $upper:tt; $rep:tt], $($rest:tt)*) => {
+        let mut $name = rand::seq::IteratorRandom::choose_multiple($lower..$upper + 1, $rng, $rep);
+        gen! {
+            @rng [$rng]
+            $($rest)*
         }
     };
 
-    ($($rest:tt)*) => {
+    // ficed value
+    (@rng [$rng:expr] $name:tt = $val:tt) => {
+        let $name = $val;
+    };
+    (@rng [$rng:expr] $name:tt = $val:tt, $($rest:tt)*) => {
+        let $name = $val;
+        gen! {
+            @rng [$rng]
+            $($rest)*
+        }
+    };
+    (@rng mut [$rng:expr] mut $name:tt = $val:tt) => {
+        let $name = $val;
+    };
+    (@rng [$rng:expr] mut $name:tt = $val:tt, $($rest:tt)*) => {
+        let mut $name = $val;
+        gen! {
+            @rng [$rng]
+            $($rest)*
+        }
+    };
+
+    // excluded range
+    (@rng [$rng:expr] $name:tt = $lower:tt..$upper:tt) => {
+        let $name = rand::Rng::gen_range($rng, $lower, $upper);
+    };
+    (@rng [$rng:expr] $name:tt = $lower:tt..$upper:tt, $($rest:tt)*) => {
+        let $name = rand::Rng::gen_range($rng, $lower, $upper);
+        gen! {
+            @rng [$rng]
+            $($rest)*
+        }
+    };
+    (@rng [$rng:expr] mut $name:tt = $lower:tt..$upper:tt) => {
+        let $name = rand::Rng::gen_range($rng, $lower, $upper);
+    };
+    (@rng [$rng:expr] mut $name:tt = $lower:tt..$upper:tt, $($rest:tt)*) => {
+        let $name = rand::Rng::gen_range($rng, $lower, $upper);
+        gen! {
+            @rng [$rng]
+            $($rest)*
+        }
+    };
+
+    // included range
+    (@rng [$rng:expr] $name:tt = $lower:tt..=$upper:tt) => {
+        let $name = rand::Rng::gen_range($rng, $lower, $upper + 1);
+    };
+    (@rng [$rng:expr] $name:tt = $lower:tt..=$upper:tt, $($rest:tt)*) => {
+        let $name = rand::Rng::gen_range($rng, $lower, $upper + 1);
+        gen! {
+            @rng [$rng]
+            $($rest)*
+        }
+    };
+    (@rng [$rng:expr] mut $name:tt = $lower:tt..=$upper:tt) => {
+        let $name = rand::Rng::gen_range($rng, $lower, $upper + 1);
+    };
+    (@rng [$rng:expr] mut $name:tt = $lower:tt..=$upper:tt, $($rest:tt)*) => {
+        let $name = rand::Rng::gen_range($rng, $lower, $upper + 1);
+        gen! {
+            @rng [$rng]
+            $($rest)*
+        }
+    };
+
+    ($($rest:tt)+) => {
         let mut rng = rand::thread_rng();
-        $crate::gen! {
+        gen! {
             @rng [&mut rng]
-            @rest, $($rest),*
+            $($rest)+
         }
         drop(rng);
-    }
-}
-
-#[test]
-fn feature() {
-    gen! {
-        a = 10,
-        b = 1..2,
-        c = [3..100; 9]
-    }
+    };
 }
