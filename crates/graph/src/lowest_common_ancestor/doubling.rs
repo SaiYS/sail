b6df91594424345c95eaaa2049859145d385d261
SaiYS <rr.kalgo@gmail.com>
2022-05-04 11:55:33 +0900

    add lca_doubling

---
+++ crates/graph/src/lowest_common_ancestor/doubling.rs
@@ -0,0 +1,118 @@
+use std::ops::{Add, Sub};
+
+use itertools::Itertools;
+use num_traits::Zero;
+
+use crate::UWLGraph;
+
+pub struct LowestCommonAncestor<W> {
+    n: usize,
+    root: usize,
+    ancestor: Vec<Vec<usize>>,
+    depth: Vec<usize>,
+    dist: Vec<W>,
+}
+
+impl<W: Copy + Add<Output = W> + Sub<Output = W> + Zero> LowestCommonAncestor<W> {
+    pub fn new(n: usize, root: usize, edges: &[(usize, usize, W)]) -> Self {
+        let tree = UWLGraph::from_edges(n, &edges);
+        let mut parent = vec![root; n];
+        let mut depth = vec![0usize; n];
+        let mut dist = vec![W::zero(); n];
+        let mut visited = vec![false; n];
+        let mut stack = vec![(root, root)];
+        while let Some((cur, prev)) = stack.pop() {
+            visited[cur] = true;
+            parent[cur] = prev;
+            for &(next, w) in tree.list[cur].iter().filter(|&&(x, _)| !visited[x]) {
+                depth[next] = depth[cur] + 1;
+                dist[next] = dist[cur] + w;
+                stack.push((next, cur));
+            }
+        }
+
+        let mut double = vec![parent];
+        for k in 1.. {
+            if double.last().unwrap().iter().all(|&x| x == root) {
+                break;
+            }
+
+            let next = (0..n)
+                .map(|x| double[k - 1][double[k - 1][x]])
+                .collect_vec();
+            double.push(next);
+        }
+
+        Self {
+            n,
+            root,
+            ancestor: double,
+            depth,
+            dist,
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        self.n
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+
+    pub fn root(&self) -> usize {
+        self.root
+    }
+
+    pub fn lca(&self, mut a: usize, mut b: usize) -> usize {
+        let mut da = self.depth[a];
+        let mut db = self.depth[b];
+        if da > db {
+            std::mem::swap(&mut a, &mut b);
+            std::mem::swap(&mut da, &mut db);
+        }
+
+        let mut d = db - da;
+        let mut i = 0;
+        while d != 0 {
+            if d & 1 != 0 {
+                b = self.ancestor[i][b];
+            }
+            d >>= 1;
+            i += 1;
+        }
+
+        if a == b {
+            a
+        } else {
+            for k in (0..self.ancestor.len()).rev() {
+                if self.ancestor[k][a] != self.ancestor[k][b] {
+                    a = self.ancestor[k][a];
+                    b = self.ancestor[k][b];
+                }
+            }
+            debug_assert_eq!(self.ancestor[0][a], self.ancestor[0][b]);
+            self.ancestor[0][a]
+        }
+    }
+
+    pub fn distance(&self, a: usize, b: usize) -> W {
+        self.dist[a] + self.dist[b] - self.dist[self.lca(a, b)] - self.dist[self.lca(a, b)]
+    }
+}
+
+#[test]
+fn debug() {
+    let edges = [(0, 1, 1), (0, 2, 1), (1, 3, 1), (1, 4, 1), (3, 5, 1)];
+    let lca = LowestCommonAncestor::new(6, 0, &edges);
+
+    assert_eq!(lca.lca(2, 3), 0);
+    assert_eq!(lca.lca(3, 4), 1);
+    assert_eq!(lca.lca(3, 5), 3);
+    assert_eq!(lca.lca(5, 4), 1);
+
+    assert_eq!(lca.distance(0, 2), 1);
+    assert_eq!(lca.distance(1, 2), 2);
+    assert_eq!(lca.distance(0, 3), 2);
+    assert_eq!(lca.distance(4, 5), 3);
+}
---
+++ crates/graph/src/lowest_common_ancestor/rmq.rs
@@ -0,0 +1,123 @@
+use crate::UWLGraph;
+use itertools::{izip, Itertools};
+use num_traits::Zero;
+use segment_tree::{segment_tree::SegmentTree, Min};
+use std::ops::{Add, Sub};
+
+#[derive(Clone)]
+pub struct LowestCommonAncestor<W> {
+    n: usize,
+    root: usize,
+    dist: Vec<W>,
+    first_appear: Vec<usize>,
+    rmq: SegmentTree<Min<(usize, usize)>>,
+}
+
+impl<W: Copy + Add<Output = W> + Sub<Output = W> + Zero> LowestCommonAncestor<W> {
+    pub fn new(n: usize, root: usize, edges: &[(usize, usize, W)]) -> Self {
+        let mut tree = UWLGraph::<W>::new(n);
+        for &(u, v, c) in edges {
+            tree.add_edge(u, v, c);
+        }
+        let mut dist = vec![W::zero(); n];
+
+        let mut depth = vec![];
+        let mut eular_tour = vec![];
+        let mut first_appear = vec![None; n];
+
+        fn dfs<W: Copy + Add<Output = W> + Zero>(
+            g: &UWLGraph<W>,
+            eular_tour: &mut Vec<usize>,
+            depth: &mut Vec<usize>,
+            first_appear: &mut [Option<usize>],
+            dist: &mut Vec<W>,
+            cur: usize,
+            prev: Option<usize>,
+            d: usize,
+            w: W,
+        ) {
+            first_appear[cur] = Some(eular_tour.len());
+            eular_tour.push(cur);
+            depth.push(d);
+            if let Some(p) = prev {
+                dist[cur] = dist[p] + w;
+            }
+            for &(next, next_w) in g.list[cur].iter().filter(|&&(x, _)| Some(x) != prev) {
+                dfs(
+                    g,
+                    eular_tour,
+                    depth,
+                    first_appear,
+                    dist,
+                    next,
+                    Some(cur),
+                    d + 1,
+                    next_w,
+                );
+                eular_tour.push(cur);
+                depth.push(d);
+            }
+        }
+
+        dfs(
+            &tree,
+            &mut eular_tour,
+            &mut depth,
+            &mut first_appear,
+            &mut dist,
+            root,
+            None,
+            0,
+            Zero::zero(),
+        );
+
+        Self {
+            n,
+            root,
+            dist,
+            first_appear: first_appear.into_iter().flatten().collect_vec(),
+            rmq: SegmentTree::from(izip!(depth, eular_tour).collect_vec()),
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        self.n
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+
+    pub fn root(&self) -> usize {
+        self.root
+    }
+
+    pub fn lca(&self, a: usize, b: usize) -> usize {
+        let mut a = self.first_appear[a];
+        let mut b = self.first_appear[b];
+        if b < a {
+            std::mem::swap(&mut a, &mut b);
+        }
+        self.rmq.range(a..=b).1
+    }
+
+    pub fn distance(&self, a: usize, b: usize) -> W {
+        self.dist[a] + self.dist[b] - self.dist[self.lca(a, b)] - self.dist[self.lca(a, b)]
+    }
+}
+
+#[test]
+fn lca_test() {
+    let edges = [(0, 1, 1), (0, 2, 1), (1, 3, 1), (1, 4, 1), (3, 5, 1)];
+    let lca = LowestCommonAncestor::new(6, 0, &edges);
+
+    assert_eq!(lca.lca(2, 3), 0);
+    assert_eq!(lca.lca(5, 4), 1);
+    assert_eq!(lca.lca(3, 5), 3);
+    assert_eq!(lca.lca(3, 4), 1);
+
+    assert_eq!(lca.distance(0, 2), 1);
+    assert_eq!(lca.distance(1, 2), 2);
+    assert_eq!(lca.distance(0, 3), 2);
+    assert_eq!(lca.distance(4, 5), 3);
+}
