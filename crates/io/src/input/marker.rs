0e46611986d7f98a745c2d74d44da06155e1a45c
SaiYS <rr.kalgo@gmail.com>
2022-07-05 05:28:49 +0900

    refactor

--- crates/io/src/input/marker.rs
+++ crates/io/src/input/marker.rs
@@ -3,15 +3,15 @@
 use crate::proconio::{__Readable as Readable, source::Source};
 use std::io::BufRead;
 
-/// Parse a number in 10-radix into Vec of a dight 0 to 9.
+/// Parse a number in 10-radix into Vec of decimal dights.
 ///
-/// Espacially this is useful when you want to parse a number
+/// This is useful when you want to parse a number
 /// that is too big to express in fixd bit-size.
 pub enum Digits {}
 
 impl Readable for Digits {
-    type Output = Vec<usize>;
-    fn read<R: BufRead, S: Source<R>>(source: &mut S) -> Vec<usize> {
+    type Output = Vec<u8>;
+    fn read<R: BufRead, S: Source<R>>(source: &mut S) -> Vec<u8> {
         source
             .next_token_unwrap()
             .chars()
@@ -19,7 +19,7 @@ impl Readable for Digits {
                 x.to_digit(10).expect(concat!(
                     "failed to parse input to digits",
                     " check if the input number contains some character except 0-9"
-                )) as usize
+                )) as u8
             })
             .collect()
     }
